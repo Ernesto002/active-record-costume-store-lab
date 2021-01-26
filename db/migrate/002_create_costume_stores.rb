7807b27bfcf042ad6470fb9b4d7192f4941e5310
ernesto <ernesto002@icloud.com>
2021-01-25 17:46:35 -0800

    commit tree 50feb1d8e93fe7c6dc652b99ba167960946fa5b1

--- db/migrate/002_create_costume_stores.rb
+++ db/migrate/002_create_costume_stores.rb
@@ -10,6 +10,7 @@ class CreateCostumeStores < ActiveRecord::Migration[5.2]
             t.integer :costume_inventory
             t.integer :num_of_employess 
             t.boolean :still_in_business
+            t.datetime :opening_time
             t.datetime :closing_time
         end
     end
